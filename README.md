928c847af8221b258f0f26dc8db8b06b3bd1f525
Leo Voon <voonlihhaur@gmail.com>
2023-09-07 20:27:20 +0800

    init: upload initial v0.1

---
+++ README.md
@@ -0,0 +1,15 @@
+# 😌 4-7-8 Breathing
+
+A website that helps you to do the 4-7-8 breathing exercise.
+
+## 🧞 Commands
+
+```bash
+pnpm install
+```
+
+Running development server:
+
+```bash
+pnpm dev
+```
---
+++ src/components/Start.tsx
@@ -0,0 +1,10 @@
+export default function Start() {
+  function handleStart() {
+    // TODO: Start the breathing exercise
+  }
+  return (
+    <a href="1-breath-out" onClick={handleStart}>
+      Start
+    </a>
+  );
+}
